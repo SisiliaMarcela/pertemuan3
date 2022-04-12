e33a0eddf65e3bdc9138057b1fc3e078eebd30a6
Sisilia Marcela <102360647+SisiliaMarcela@users.noreply.github.com>
2022-04-12 17:03:53 +0700

    AdminLTE

--- resources/views/layout/navbarmenu.blade.php
+++ resources/views/layout/navbarmenu.blade.php
@@ -1,14 +1,12 @@
 <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/Mahasiswa">List Mahasiswa</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/Mahasiswa/create">Tambah Mahasiswa</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link disabled">Disabled</a>
-              </li>
+    <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="/">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/Mahasiswa">List Mahasiswa</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/Mahasiswa/create">Tambah Mahasiswa</a>
+      </li>
+
 </ul>
